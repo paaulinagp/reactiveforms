86eecc179d607fab6f079e6e01d865134caf71ef
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-24 22:17:53 -0500

    feat: agregando formularios reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +9,13 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
 
 @NgModule({
   declarations: [AppComponent, TemplateComponent, ReactiveComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryService } from 'src/app/services/country.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private countryService: CountryService) {
+    this.createForm();
   }
 
+  ngOnInit(): void {}
+
+  createForm(): void {
+    this.form = this.fb.group({
+      name: ['Pau', [Validators.required, Validators.minLength(5)]],
+      lastName: ['Guerrero', [Validators.required, Validators.minLength(5)]],
+      email: [
+        'pau@mail.com',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form);
+  }
 }
